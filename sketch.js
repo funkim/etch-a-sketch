61baa34787f32230535d8afbc62d7dab57f14d08
funkim <mcking172@skiff.com>
2024-02-07 20:07:16 -0500

    Making it so the amount of divs set inside where the Etch-a-sketch will be is equal to the amount wanted by the user.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,15 @@
 
 const start = document.createElement('div');
-document.appendChild.start;
+document.body.appendChild(start);
+let wantedAmount = 1;
+const blocks = document.createElement('div') 
+blocks.style.width = 200;
+blocks.style.height = 200;
+blocks.id = "block";
+const cloneBlock = blocks.cloneNode;
+
+let amountOfBlocks = document.querySelectorAll("block");
+while (amountOfBlocks.length < wantedAmount) {
+    start.appendChild(cloneBlock);
+
+}

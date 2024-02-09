50315435316a31094b7c2a9f20e6d404899b5f65
funkim <mcking172@skiff.com>
2024-02-08 21:17:57 -0500

    it works

--- sketch.js
+++ sketch.js
@@ -10,12 +10,21 @@ displayContainer.id = "display";
 
 document.body.appendChild(displayContainer);
 displayContainer.appendChild(container)
-container.appendChild(block);
 
 
-let wantedAmount = 256;
+let wantedAmount = 1028;
 
 for (let i = 0; i < wantedAmount; i++) {
     const cloneBlock = block.cloneNode()
+    cloneBlock.style.backgroundColor = 'white';
     container.appendChild(cloneBlock);
 }
+
+const blocksSelected = document.querySelectorAll(".block")
+    blocksSelected.forEach(block => {
+    block.addEventListener('mouseover', (e) => {
+        if(e.target.style.backgroundColor === 'white') {
+            e.target.style.backgroundColor = 'red'
+        }
+    })
+});
